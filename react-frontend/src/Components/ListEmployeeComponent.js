fa0010abcab649fa7562cc2f366edce16022951e
MrUnwonted <arjungodzarjun@gmail.com>
2023-08-05 20:58:42 +0530

    List fixed

--- react-frontend/src/Components/ListEmployeeComponent.js
+++ react-frontend/src/Components/ListEmployeeComponent.js
@@ -6,35 +6,42 @@ const ListEmployeeComponent = () => {
     const [employees, setEmployees] = useState([])
 
     useEffect(() => {
-        EmployeeService.getEmployees().then((response) => {
+
+        getAllEmployees();
+    }, [])
+
+    const getAllEmployees = () => {
+        EmployeeService.getAllEmployees().then((response) => {
             setEmployees(response.data)
             console.log(response.data);
-        }).catch(error => {
-            console.log(error);})
-    }, [])
-    
+        }).catch(error =>{
+            console.log(error);
+        })
+    }
 
   return (
     <div className='container'>
       <h2 className='text-center'>List Employees</h2>
       <table className='table table-bordered table-stripped'>
         <thead>
+            <tr>
             <th> Employee Id</th>
             <th> Employee first Name</th>
             <th> Employee last Name</th>
             <th> Employee Email Id</th>
+            </tr>
         </thead>
         <tbody>
             {
                 employees.map(
-                    employee =>{
+                    employee =>
                     <tr key={employee.id}>
                         <td>{employee.id} </td>
                         <td>{employee.firstName} </td>
                         <td>{employee.lastName} </td>
                         <td>{employee.emailId} </td>
                     </tr>
-                })
+                )
             }
          </tbody>
       </table>
--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
-const EMPLOYEE_BASE_REST_API_URL = 'http://localhost:8080/api/v1/employees'
+const EMPLOYEE_BASE_REST_API_URL = 'http://localhost:8080/api/v1/employees';
 
 class EmployeeService{
-    getEmployees(){
+
+     getAllEmployees(){
         return axios.get(EMPLOYEE_BASE_REST_API_URL)
     }
         
